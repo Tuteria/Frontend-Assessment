6d082bf63d5b9f18aa12306d5bbffe440e8d971b
Aleem Isiaka <aleemisiaka@gmail.com>
2020-08-29 20:29:20 +0100

    feat: implementation for delete note by nodeid

--- services/app/src/notes.ts
+++ services/app/src/notes.ts
@@ -32,7 +32,19 @@ router.put("/:nodeId", async (req, res) => {
 	});
 	res.status(200).json(result);
 });
+
 // DELETE /notes/:note-id The ability to delete a specific anonymous note
+router.delete("/:nodeId", async (req, res) => {
+	const prisma: PrismaClient = req.app.locals.prisma;
+	const { description, title } = req.body;
+	const result = await prisma.notes.delete({
+		where: {
+			id: parseInt(req.params.nodeId),
+		},
+	});
+	res.status(200).json(result);
+});
+
 // POST /users/create Endpoint to create a user
 // GET /users/:username/notes Fetching the notes of a particular user
 
