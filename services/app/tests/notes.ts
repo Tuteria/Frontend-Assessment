d40aaa917c67f1e3de067f1ba97c000ac462fdf9
Aleem Isiaka <aleemisiaka@gmail.com>
2020-08-29 20:28:03 +0100

    feat: test for delete note with nodeid

--- services/app/tests/notes.ts
+++ services/app/tests/notes.ts
@@ -48,7 +48,7 @@ Notes("Get endpoint works as expected", async (context) => {
 		});
 });
 
-Notes("Create endpoint works as expected", async (context) => {
+Notes("Update endpoint works as expected", async (context) => {
 	const note = {
 		title: "Sample Update Note",
 		description: "This is a sample update note description",
@@ -75,6 +75,30 @@ Notes("Create endpoint works as expected", async (context) => {
 		.expect("Content-Type", /json/);
 	assert.is(updateResponse.body.description, noteUpdate.description);
 	assert.is(updateResponse.body.title, noteUpdate.title);
+});
+
+Notes("Delete endpoint works as expected", async (context) => {
+	const note = {
+		title: "Sample Update Note",
+		description: "This is a sample update note description",
+	};
+	// Creates the note
+	const response = await request(App)
+		.post("/notes/create")
+		.send(note)
+		.set("Accept", "application/json")
+		.expect("Content-Type", /json/);
+
+	assert.is(response.body.title, note.title);
+	assert.ok(response.body.id);
+
+	// Creates the note
+	const updateResponse = await request(App)
+		.delete(`/notes/${response.body.id}`)
+		.set("Accept", "application/json")
+		.expect("Content-Type", /json/);
+	assert.is(updateResponse.body.description, note.description);
+	assert.is(updateResponse.body.title, note.title);
 });
 
 Notes.run();
