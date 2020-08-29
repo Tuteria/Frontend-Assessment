527c7b70a2ffabec1eac5487984f930baf6fc0da
Aleem Isiaka <aleemisiaka@gmail.com>
2020-08-29 19:28:45 +0100

    feat: base folder for the test api service

---
+++ services/app/src/notes.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/create", async (req, res) => {
+	const prisma: PrismaClient = req.app.locals.prisma;
+	const { description, title } = req.body;
+	const result = await prisma.notes.create({
+		data: { description, title },
+	});
+	res.status(200).json(result);
+});
+
+// POST /notes/create Anonymous note creation
+// GET /notes Fetching the list of anonymous notes created
+// PUT /notes/:note-id The ability to update a specific anonymous note
+// DELETE /notes/:note-id The ability to delete a specific anonymous note
+// POST /users/create Endpoint to create a user
+// GET /users/:username/notes Fetching the notes of a particular user
+
+export default router;
