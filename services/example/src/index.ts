549761c71708f5d2fcbb269c7ac23d1ed87279d4
Beesama <gbozee@gmail.com>
2020-08-31 18:40:17 +0100

    save changes

--- services/example/src/index.ts
+++ services/example/src/index.ts
@@ -6,6 +6,7 @@ import { logging as httpLogger } from "@tuteria/common/src/middleware";
 const app = express();
 app.use(bodyParser.json());
 app.use(httpLogger);
+
 app.use("/notes", notes);
 
 export default app;
