beae692be01f5e1ad00dbb5993b9f6da2c32d9bf
amjedidiah <imunacode@gmail.com>
2020-09-02 12:38:56 +0100

    chore: Completed bonus for first task

--- data-layer/data_layer/settings.py
+++ data-layer/data_layer/settings.py
@@ -2,5 +2,5 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.join(BASE_DIR, "..", "..", "example.db")
-DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:///{ROOT_DIR}")
+DATABASE_URL = "postgres://smmuymuwdjdnwh:d32a41d2a517936b68388f2236d12e4680a3ea6e6bd42bfebc70aa3cde7e358e@ec2-52-71-85-210.compute-1.amazonaws.com:5432/dbekvrbcb0rauv"
 print(DATABASE_URL)
