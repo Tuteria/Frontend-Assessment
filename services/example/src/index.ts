b968fa28b080f51ee466c69fc65f5a782eb69f7c
Segun <shegunolanitori@gmail.com>
2020-09-05 01:46:02 +0100

    refactor: uninstalled cors from the example directory, installed in root package.json

--- services/example/src/index.ts
+++ services/example/src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import notes from "./notes";
 import users from "./users";
-import * as bodyParser from "body-parser";
 import cors from "cors";
 import { logging as httpLogger } from "../../../packages/common/src/middleware";
+// Err Msg: Cannot find module '@tuteria/common/src/middleware' or its corresponding type declarations.
 // import { logging as httpLogger } from "@tuteria/common/src/middleware";
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(httpLogger);
 app.use(
 	cors({
