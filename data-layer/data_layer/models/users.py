343f8c4c26b001fb032b15510ee1118e329373a2
japhethca <chidiebere.anyigor@gmail.com>
2020-09-08 15:49:39 +0100

    chore: update db users columns - added new migrations - updated prisma schema file

--- data-layer/data_layer/models/users.py
+++ data-layer/data_layer/models/users.py
@@ -5,8 +5,9 @@ import sqlalchemy as sql
 class Users(Base):
     __tablename__ = "users"
     id = sql.Column(sql.Integer, primary_key=True)
-    username = sql.Column(sql.String)
+    username = sql.Column(sql.String, unique=True)
     password = sql.Column(sql.String)
+    is_admin = sql.Column(sql.Boolean, default=False)
 
     def __repr__(self):
         return f"<User (id={self.id}, username={self.username})>"
