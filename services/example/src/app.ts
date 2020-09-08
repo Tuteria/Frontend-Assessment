173aad42c99a4bc2b0fc867274a46642836cf94f
japhethca <chidiebere.anyigor@gmail.com>
2020-09-08 20:31:55 +0100

    fix: type errors in app.ts

--- services/example/src/app.ts
+++ services/example/src/app.ts
@@ -9,7 +9,7 @@ let PORT = 3000;
 if (process.env.PORT !== undefined) {
 	PORT = parseInt(process.env.PORT, 10);
 }
-app.listen(PORT, (err: Error) => {
+app.listen(PORT, (err?: Error) => {
 	if (err) throw err;
 	console.log(`> Running on localhost:${PORT}`);
 });
