a49594da4fa8c8c6087eaf56bcda65065ffcdfac
Anyigor Chidiebere <chidiebere.anyigor@gmail.com>
2020-09-08 02:33:33 +0100

    chore: updated migration file (#1)
    - configure schema.prisma to use postgres
    - change postgres user,pass and db names in docker-compose.yml

--- data-layer/migrations/versions/d096749ea13c_added_notes_table.py
+++ data-layer/migrations/versions/d096749ea13c_added_notes_table.py
@@ -1,8 +1,8 @@
 """Added notes table
 
-Revision ID: 0514e3418c6b
+Revision ID: d096749ea13c
 Revises: 
-Create Date: 2020-08-20 10:17:31.453707
+Create Date: 2020-09-08 01:50:26.358067
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0514e3418c6b'
+revision = 'd096749ea13c'
 down_revision = None
 branch_labels = None
 depends_on = None
