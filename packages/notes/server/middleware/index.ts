9e0b0591dbe4eb9d0282373ab2fe53037e80b67d
Malik Mahmud <almalikmahmud@gmail.com>
2020-09-08 20:36:39 +0100

    refactor: update request auth middleware

--- packages/notes/server/middleware/index.ts
+++ packages/notes/server/middleware/index.ts
@@ -3,38 +3,27 @@ import jwt, { VerifyErrors } from "jsonwebtoken";
 import { prisma } from "../lib";
 import { CustomRequest } from "../types";
 
-const ADMIN_PATH = "/admin";
-
 export default {
 	authenticateRequest(req: CustomRequest, res: Response, next: NextFunction) {
 		const { token } = req.cookies;
-		if (!token) {
-			if (req.path === ADMIN_PATH) res.redirect("/login");
-			else {
-				req.user = null;
-				next();
-			}
-		} else {
+		if (!token) req.user = null;
+		else {
 			jwt.verify(
 				token,
 				process.env.JWT_SECRET,
 				async (err: VerifyErrors, decoded) => {
 					if (err) {
-						if (req.path === ADMIN_PATH) res.redirect("/login");
-						else {
-							req.user = null;
-							next();
-						}
+						req.user = null
 					} else {
 						const user = await prisma.users.findOne({
 							where: { id: decoded.id },
 						});
 						req.user = user;
-						next();
 					}
 				}
 			);
 		}
+		next();
 	},
 
 	async verifyNoteOwnership(
