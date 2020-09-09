c415feda5ab34f66f44e1366537c20b5c11c05e2
Malik Mahmud <almalikmahmud@gmail.com>
2020-09-09 17:18:01 +0100

    test: add tests for [POST /api/notes] endpoint

--- packages/notes/__tests__/notes.ts
+++ packages/notes/__tests__/notes.ts
@@ -1,9 +1,13 @@
 import request from "supertest";
+import session from "supertest-session";
 import app from "../server/app";
 import { prisma } from "../server/lib";
 
-afterAll((done) => {
-	prisma.$disconnect().then(done).catch(done);
+const userSession = session(app);
+
+afterAll(async () => {
+	await prisma.$executeRaw("DELETE FROM notes");
+	await prisma.$disconnect();
 });
 
 describe("GET /api/notes", () => {
@@ -17,10 +21,13 @@ describe("GET /api/notes", () => {
 	});
 });
 
-
 describe("GET /api/notes/:id", () => {
+	let note;
 	it("should return a note with the matching id", async () => {
-		const response = await request(app).get("/api/notes/1");
+		note = await request(app)
+			.post("/api/notes")
+			.send({ title: "I", description: "Random words" });
+		const response = await request(app).get(`/api/notes/${note.body.id}`);
 		expect(response.status).toBe(200);
 		expect(response.body).toHaveProperty("id");
 		expect(response.body).toHaveProperty("title");
@@ -29,8 +36,42 @@ describe("GET /api/notes/:id", () => {
 	});
 
 	it("should return {} if a note with the id does not exist", async () => {
-		const response = await request(app).get("/api/notes/1000");
+		const response = await request(app).get(`/api/notes/${+note.body.id + 1}`);
 		expect(response.status).toBe(200);
 		expect(response.body).toMatchObject({});
 	});
-})
+});
+
+describe("POST /api/notes", () => {
+	it("should create an anonymous note if user is not logged in", async () => {
+		const response = await request(app).post("/api/notes").send({
+			title: "Sample post",
+			description: "This is a sample post",
+		});
+		expect(response.status).toBe(201);
+		expect(response.body).toHaveProperty("title", "Sample post");
+		expect(response.body).toHaveProperty(
+			"description",
+			"This is a sample post"
+		);
+	});
+
+	it("should create a note with an author id if user is logged in", async () => {
+		const user = await userSession
+			.post("/api/auth/login")
+			.send({ username: "malik", password: "password" });
+		const response = await userSession
+			.post("/api/notes")
+			.send({ title: "I", description: "Yet another sample post" });
+		expect(response.status).toBe(201);
+		expect(response.body).toHaveProperty("author_id", user.body.id);
+	});
+
+	it("should not create a user with malformed input", async () => {
+		const response = await request(app)
+			.post("/api/notes")
+			.send({ title: 1, description: "Sample post" });
+		expect(response.status).toBe(400);
+		expect(response.body).toHaveProperty("message", '"title" must be a string');
+	});
+});
