1190923337758102104499f26726d61f27918533
Chidi Enwereji <chidibede@gmail.com>
2020-09-12 11:47:20 +0100

    fix: removed unused codes

--- packages/www/pages/admin/logout.tsx
+++
@@ -1,8 +0,0 @@
-import nookies, { destroyCookie } from "nookies";
-
-export const logout = async ({ res }) => {
-	destroyCookie(null, "sbsToken", {
-		path: "/", // THE KEY IS TO SET THE SAME PATH
-	});
-	return { ok: true };
-};
