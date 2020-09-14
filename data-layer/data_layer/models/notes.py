25ca3bd6556397550d1069c52ecd61af9bd48198
Folajimi <bekomay26@gmail.com>
2020-09-14 17:55:21 +0100

    feat: modify notes model

--- data-layer/data_layer/models/notes.py
+++ data-layer/data_layer/models/notes.py
@@ -1,5 +1,7 @@
 from data_layer.models.base import Base
 import sqlalchemy as sql
+from sqlalchemy.orm import relationship
+from .users import Users
 
 
 class Notes(Base):
@@ -7,6 +9,8 @@ class Notes(Base):
     id = sql.Column(sql.Integer, primary_key=True)
     title = sql.Column(sql.String)
     description = sql.Column(sql.Text)
+    user_id = sql.Column(sql.Integer, sql.ForeignKey('users.id'))
+    user = relationship(Users)
 
     def __repr__(self):
         return f"<Note (id={self.id}, title={self.title})>"
