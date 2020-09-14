9328eab612b988f693d0e9591fd3a82caff0e0c2
Folajimi <bekomay26@gmail.com>
2020-09-14 23:11:03 +0100

    feat: add admin page

--- packages/www/pages/admin/index.tsx
+++ packages/www/pages/admin/index.tsx
@@ -1,25 +1,15 @@
-import Head from 'next/head';
-import useSwr, {mutate} from 'swr';
+import useSwr from 'swr';
 import Link from 'next/link'
-import axios from 'axios';
-import { useRouter } from 'next/router';
-
-import { useToast } from "@chakra-ui/core";
 import {useContext} from "react";
-import NoteList from "@tuteria/components/src/ui-elements/noteList";
-import Layout from "../../src/layout";
-import AuthContext, {getAuthToken} from "../../utils/auth";
-import {Table} from "@tuteria/components/src/ui-elements/table";
+import AuthContext from "../../utils/auth";
+import Layout from "../../src/layout/layout";
+import NoteList from "@tuteria/components/src/ui-elements/noteList/noteList";
+import Table from "@tuteria/components/src/ui-elements/table/table";
 
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
-// const fetcher = (url) => fetch(url, { headers: { Authorization: `Bearer ${getAuthToken()}`}}).then((res) => res.json())
-// const fetcher = (url) => axios.get(url, { headers: { Authorization: `Bearer ${getAuthToken()}`}}).then((res) => res.json());
-// const fetcher = (url) => axios.get(url).then((res) => res.json());
 
  const AdminPage = ({children}) => {
-	const router = useRouter();
-	const toast = useToast();
 	let isLoading = false;
 	let users = [];
 
@@ -34,13 +24,31 @@ const fetcher = (url) => fetch(url).then((res) => res.json())
 
 	if (!isAdmin) {
 		return (
-			<div className="notFound1">
+			<div className="notFound">
 				<p>Page not found.</p>
 				<p>Go
 					<Link href="/">
 						<a>Home</a>
 					</Link>
 				</p>
+				<style jsx>{`
+        .notFound {
+					height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+				}
+
+				.notFound p {
+          font-size: 24px;
+				}
+
+				.notFound a {
+          color: blue;
+          margin-left: 10px;
+				}
+      `}</style>
 			</div>
 		);
 	}
@@ -60,6 +68,11 @@ const fetcher = (url) => fetch(url).then((res) => res.json())
 						<Table onRowViewClick={onViewClick} users={users} loading={isLoading} />
 				)}
 			/>
+			<style jsx>{`
+        .adminContainer {
+          width: 100%;
+				}
+      `}</style>
 		</div>
 	)
 }
