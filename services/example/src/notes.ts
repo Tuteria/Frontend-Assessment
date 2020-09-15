d6bbd3a5cbec324cfa865df810455986d5f641a5
Deji Adegbite <d2alphame@yahoo.com>
2020-09-15 13:12:01 +0100

    fix(package.json): Fixed build under scripts making builds to fail. The script 'build' command in the package.json file was changed to 'lerna run build'. The original 'lerna build' was causing builds to fail. First task also done. All tests pass

--- services/example/src/notes.ts
+++ services/example/src/notes.ts
@@ -12,10 +12,17 @@ router.post("/create", async (req, res) => {
 	res.status(200).json(result);
 });
 
+// router.get("/", async(req, res) => {
+// 	const prisma: PrismaClient = req.app.locals.prisma;
+// 	const result = await prisma.notes.findMany();
+// 	res.json(result);
+// });
 
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
 	const prisma: PrismaClient = req.app.locals.prisma;
-	const result = await prisma.notes.findMany();
+	const result = await prisma.notes.findMany({
+		where: { title: "Second note" },
+	});
 	res.json(result);
 });
 
