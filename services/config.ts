c8e305f15d15b748dc3499b5b6f6917c501d1519
Temitope-Emmanuel <temitopeojo0@gmail.com>
2020-09-21 22:35:37 +0100

    feat: :sparkles: Added populating script
    Added means to populate db with hindering prod / dev

--- services/config.ts
+++ services/config.ts
@@ -1,7 +1,7 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3000,
-  SERVER_URL: proces `http://localhost:${process.env.PORT || 3000}`,
+  SERVER_URL: process.env.SERVER_URL || `http://localhost:${process.env.PORT || 3000}`,
   jwtSecret: process.env.jwtSecret || "Make person no tell me say i too lazy",
   TEST_DATABASE_URL:process.env.TEST_DATABASE_URL
 }
